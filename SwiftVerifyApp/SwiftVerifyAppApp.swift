2966890beef0119731475db2262f70bf8ff59986
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2020-11-06 13:58:38 +0000

    Initial Commit

---
+++ SwiftVerifyApp/SwiftVerifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftVerifyAppApp.swift
+//  SwiftVerifyApp
+//
+//  Created by Abdulhakim Ajetunmobi on 06/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftVerifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
